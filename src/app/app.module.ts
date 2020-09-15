8b1f407626037c63050add4579fc6951805f3cf8
onyldas <onyldas@gmail.com>
2020-09-15 19:30:34 +0200

    Footer and Header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { ParcoursComponent } from './parcours/parcours.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponentComponent
+    FooterComponentComponent,
+    HeaderComponentComponent,
+    ParcoursComponent
   ],
   imports: [
     BrowserModule,
